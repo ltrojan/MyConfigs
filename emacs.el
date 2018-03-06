53b8817d277246c9d51f2f8df95218bfcc86b62f
Lorenzo Trojan <lorenzo.trojan@gmail.com>
2018-03-06 11:20:40 +0000

    add emacs melpa packages

--- emacs.el
+++ emacs.el
@@ -1,5 +1,7 @@
 ;; remove splash screen
 (setq inhibit-splash-screen t)
 
+;; melpa and other packages
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
